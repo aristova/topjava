078f7abda1d934859bee8c2c3f19f8b6933a6dac
Aristova <you@example.com>
2021-03-14 20:56:46 +0300

    Revert "HW1. Apply patches"
    This reverts commit 3924ee8db7558ef5111003d9c878c940cd75d432.

--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExcess.java
@@ -2,37 +2,25 @@ package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
 
-public class MealTo {
+public class UserMealWithExcess {
     private final LocalDateTime dateTime;
 
     private final String description;
 
     private final int calories;
 
-//    private final AtomicBoolean excess;      // or Boolean[1],  filteredByAtomic
-//    private final Boolean excess;            // filteredByReflection
-//    private final Supplier<Boolean> excess;  // filteredByClosure
-    private boolean excess;
+    private final boolean excess;
 
-    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
+    public UserMealWithExcess(LocalDateTime dateTime, String description, int calories, boolean excess) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.excess = excess;
     }
 
-//    public Boolean getExcess() {
-//        return excess.get();
-//    }
-
-    // for filteredBySetterRecursion
-    public void setExcess(boolean excess) {
-        this.excess = excess;
-    }
-
     @Override
     public String toString() {
-        return "MealTo{" +
+        return "UserMealWithExcess{" +
                 "dateTime=" + dateTime +
                 ", description='" + description + '\'' +
                 ", calories=" + calories +
