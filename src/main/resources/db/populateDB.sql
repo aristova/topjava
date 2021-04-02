daa5d4f63630ec0d54bd2e3d022335263a985009
Aristova <you@example.com>
2021-04-02 10:46:41 +0300

    HW3. Add DB scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password)
@@ -9,3 +10,7 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id)
+VALUES ('2021-02-11 09:00:00', 'Завтрак', 300, 100000),
+       ('2021-02-13 14:00:00', 'Обед', 400, 100001);
