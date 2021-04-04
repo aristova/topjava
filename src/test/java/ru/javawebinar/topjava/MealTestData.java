d458eac6e89f4dceb56cecc177e2117034691e4d
Aristova <you@example.com>
2021-04-04 19:26:01 +0300

    HW3. Add tests for JdbcMealRepository

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealTestData {
+    public static final int MEAL_ID = START_SEQ + 2;
+    public static final int NOT_FOUND = 10;
+
+    public static final Meal meal = new Meal(MEAL_ID, LocalDateTime.now(), "Завтрак", 300);
+
+    public static Meal getNew() {
+        return new Meal(null, LocalDateTime.now(), "Завтрак", 300);
+    }
+
+    public static Meal getUpdated() {
+        Meal updated = new Meal(meal);
+        updated.setDateTime(LocalDateTime.now());
+        updated.setDescription("UpdatedMeal");
+        updated.setCalories(500);
+        return updated;
+    }
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).usingRecursiveComparison().ignoringFields("dateTime").isEqualTo(expected);
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).usingElementComparatorIgnoringFields("dateTime").isEqualTo(expected);
+    }
+}
