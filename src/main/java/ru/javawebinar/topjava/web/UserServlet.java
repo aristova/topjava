4f4a06b5af2595f832e0bb400210b7bf717701a3
Aristova <you@example.com>
2021-04-06 13:23:11 +0300

    HW2. Add redirect insted of using MealRestController

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,12 +1,5 @@
 package ru.javawebinar.topjava.web;
 
-import org.slf4j.Logger;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.javawebinar.topjava.repository.UserRepository;
-import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
-import ru.javawebinar.topjava.web.meal.MealRestController;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,30 +7,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Objects;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
 public class UserServlet extends HttpServlet {
-    private static final Logger log = getLogger(UserServlet.class);
-    private UserRepository repository;
-
-    private MealRestController mealRestController;
-    private ConfigurableApplicationContext appCtx;
-
-    @Override
-    public void destroy() {
-        appCtx.close();
-    }
-
-    @Override
-    public void init() {
-        appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml");
-        mealRestController = appCtx.getBean(MealRestController.class);
-        repository = new InMemoryUserRepository();
-    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
 
@@ -45,7 +18,6 @@ public class UserServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String paramId = Objects.requireNonNull(request.getParameter("id"));
         SecurityUtil.setAuthUserId(Integer.parseInt(paramId));
-        request.setAttribute("meals", mealRestController.getAll());
-        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+        response.sendRedirect("meals");
     }
 }
