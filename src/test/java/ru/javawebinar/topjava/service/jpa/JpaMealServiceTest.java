a18efea98bbd9ae38942346e12c06b9dc20f0437
Daria Aristova <daria.aristova@gmail.com>
2021-05-07 20:40:10 +0300

    HW05. Spring profiles in MealServlet and SpringMain

--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaMealServiceTest.java
@@ -4,5 +4,5 @@ import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.MealServiceTest;
 
 @ActiveProfiles("jpa")
-public class JpaMealServiceTest  extends MealServiceTest {
+public class JpaMealServiceTest extends MealServiceTest {
 }
